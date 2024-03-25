64d2cac10710dd5de03eda1cd103829cac3b8667
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-03-25 17:45:39 +0200

    add filter and search and order api

--- products/api.py
+++ products/api.py
@@ -4,12 +4,15 @@ from rest_framework import generics
 from .models import Product,Brand
 from .paginations import Mypagination
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class ProductListapi(generics.ListAPIView):
     queryset=Product.objects.all()
     serializer_class=ProductListSerailzers
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
     filterset_fields = ['price',]
+    search_fields = ['name', 'subtitle']
+    ordering_fields = ['price',]
 
 class ProductDetailapi(generics.RetrieveAPIView):
     queryset=Product.objects.all()
