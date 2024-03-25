25eb4130bb35a0433bef96a9db5f317ffe773b61
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-03-25 17:36:33 +0200

    add pagination in api

--- products/api.py
+++ products/api.py
@@ -2,6 +2,7 @@ from .serializers import ProductDetailSerailizers,ProductListSerailzers
 from .serializers import BrandListSerailizers,BrandDetailSerailizers
 from rest_framework import generics
 from .models import Product,Brand
+from .paginations import Mypagination
 
 class ProductListapi(generics.ListAPIView):
     queryset=Product.objects.all()
@@ -10,10 +11,12 @@ class ProductListapi(generics.ListAPIView):
 class ProductDetailapi(generics.RetrieveAPIView):
     queryset=Product.objects.all()
     serializer_class=ProductDetailSerailizers
+    
 
 class BrandListApi(generics.ListAPIView):
     queryset=Brand.objects.all()
     serializer_class=BrandListSerailizers
+    pagination_class=Mypagination
 
 class BrandDetailapi(generics.RetrieveAPIView):
     queryset=Brand.objects.all()
--- project/settings.py
+++ project/settings.py
@@ -47,6 +47,11 @@ INSTALLED_APPS = [
     'products',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 70
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
