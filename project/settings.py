d16ef403b5da7eedf5323b8933f1adf251997a2f
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-04-03 17:08:59 +0200

    send gamil

--- project/settings.py
+++ project/settings.py
@@ -164,6 +164,14 @@ AUTHENTICATION_BACKENDS=[
     'accounts.backend.Login_email_username'
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ('mohamedelazab2017@gmail.com')
+EMAIL_HOST_PASSWORD =('srkwhtbnesinqxqo')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 
